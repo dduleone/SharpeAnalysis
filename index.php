c70a446e51678538d389fc3a6a8d2063e9fb7516
Dan Leone-Zwillinger <dantothelz@gmail.com>
2012-11-03 02:42:41 -0400

    It's actually working, now. Still have to add the aggregate Fund columns, as well as the actual analysis.

--- index.php
+++ index.php
@@ -4,61 +4,129 @@
 
 define("STOCK_COUNT", 4);
 
-function getGet($var, $index = null){
-	if(isset($_GET[$var])){
-		if( ($index !== null) && (is_array($_GET[$var])) ){
-			return $_GET[$var][$index];
-		} else {
-			return $_GET[$var];
+class Util{
+
+	const REMOTE_URL = "http://ichart.finance.yahoo.com/table.csv?a=00&b=1&c=2011&d=11&e=31&f=2011&g=d&ignore=.csv&s=%s";
+
+	const COL_DATE = 0;
+	const COL_OPEN = 1;
+	const COL_HIGH = 2;
+	const COL_LOW = 3;
+	const COL_CLOSE = 4;
+	const COL_VOLUME = 5;
+	const COL_ADJ_CLOSE = 6;
+	const COL_CUM = 7;
+	const COL_VALUE = 8;
+
+
+	static function getGet($var, $index = null){
+		return (isset($_GET[$var])) ? ( ((is_array($_GET[$var])) && ($index!==null)) ? $_GET[$var][$index] : $_GET[$var] ) : "";
+	}
+
+	static function generateUrl($symbol){
+		return sprintf(self::REMOTE_URL, $symbol);
+	}
+
+	static function getStockCount(){
+		return (isset($_GET['stockcount'])) ? $_GET['stockcount'] : false;
+	}
+
+	static function getSymbolArray(){
+		return (isset($_GET['symbol'])) ? $_GET['symbol'] : false;		
+	}
+
+	static function getCapitalArray(){
+		return (isset($_GET['capital'])) ? $_GET['capital'] : false;
+	}
+
+	static function getSymbolCount(){
+		$ary = self::getSymbolArray();
+		return ($ary !== false) ? count($ary) : 0;
+	}
+
+	static function getCapitalCount(){
+		$ary = self::getCapitalArray();
+		return ($ary !== false) ? count($ary) : 0;
+	}
+
+	static function buildRows($handle){
+		$rows = array();
+		while($row = fgetcsv($handle)){
+			$rows[] = $row;
 		}
-	} else {
-		return "";
+		return array_reverse($rows);
+	}
+
+	static function prettyPercent($raw){
+		$rounded = round($raw, 4) * 100;
+		return sprintf("%.2f%%", $rounded);
+	}
+
+	static function prettyMoney($raw){
+		$rounded = number_format($raw, 2);
+		return sprintf("$%s", $rounded);
+	}
+
+	static function cleanData($col, $value){
+		switch($col){
+			case self::COL_DATE:
+			break;
+			case self::COL_OPEN:
+			break;
+			case self::COL_HIGH:
+			break;
+			case self::COL_LOW:
+			break;
+			case self::COL_CLOSE:
+			break;
+			case self::COL_VOLUME:
+			break;
+			case self::COL_ADJ_CLOSE:
+				$value = self::prettyMoney($value);
+			break;
+			case self::COL_CUM:
+				$value = self::prettyPercent($value);
+			break;
+			case self::COL_VALUE:
+				$value = self::prettyMoney($value);
+			break;
+			default:
+			break;
+		}
+		return $value;
 	}
 }
 
-function buildRows($handle){
-	$rows = array();
-	while($row = fgetcsv($handle)){
-		$rows[] = $row;
+$print_cols = array();
+$print_cols[Util::COL_ADJ_CLOSE] = "Adj Close";
+$print_cols[Util::COL_CUM] = "Cum %";
+$print_cols[Util::COL_VALUE] = "Daily Value";
+
+
+if(Util::getStockCount()){
+	if(Util::getSymbolCount() != Util::getCapitalCount()){
+		echo "<h1 class='error'>Error! Number of Stock Symbols (".Util::getSymbolCount().") does not match number of Investment Capital Values (".Util::getCapitalCount().").</h1>";
 	}
-	$rows = array_reverse($rows);
-	return $rows;
-}
 
-$results = false;
-$years = array();
-//print_r($_GET);
-if(isset($_GET['symbol'])){
-	$results = true;
-	$table = "";
+	$symbols = Util::getSymbolArray();
+	$capitals = Util::getCapitalArray();
 
-	foreach($_GET['symbol'] as $index => $symbol){
-		if(!isset($_GET['capital'][$index]) || !is_numeric($_GET['capital'][$index])){
-			$_GET['capital'][$index] = 0;
-		}
-
-		$_url = "http://ichart.finance.yahoo.com/table.csv?a=00&b=1&c=2011&d=11&e=31&f=2011&g=d&ignore=.csv&s=" . $symbol;
-		$data_handle = fopen($_url, 'r');
+	$stocks = array();
+	foreach($symbols as $i => $symbol){
+		$data_handle = fopen(Util::generateUrl($symbol), 'r');
 		$headers = fgetcsv($data_handle);
-		$rows = buildRows($data_handle);
-		$year = new TradeYear(getGet('capital', $index));
-		foreach($rows as $row){
-			$year->addRow($row);
-		}
-		$table[] = $year->dump();
-		$years[] = $year;
+		$stocks[] = Util::buildRows($data_handle);
 	}
 
-	$buffer = "";
-	foreach($table[0] as $index => $year){
-		$a = array();
-		for($i = 0; $i < STOCK_COUNT; $i++){
-			$a[] = $table[$i][$index];
+	foreach($stocks as $i => $stock){
+		$dayone = $stock[0][Util::COL_ADJ_CLOSE];
+		$capital = $capitals[$i];
+		foreach($stock as $j => $day){
+			$dailycum = $day[Util::COL_ADJ_CLOSE] / $dayone;
+			$stocks[$i][$j][Util::COL_CUM] = $dailycum;
+			$stocks[$i][$j][Util::COL_VALUE] = $dailycum * $capital;
 		}
-		$buffer[] = implode(",", $a);
 	}
-	var_dump($buffer);
-	
 }
 ?><!DOCTYPE html>
 <html>
@@ -68,13 +136,14 @@ if(isset($_GET['symbol'])){
 	<body>
 		<div id='input_form'>
 			<form action='?' method='get'>
+				<input type='hidden' name='stockcount' value='<?php echo STOCK_COUNT; ?>' />
 <?php
 					for($i = 0; $i < STOCK_COUNT; $i++){
 ?>
 						<div class='stock'>
 							<h3>Stock <?php echo $i; ?></h3>
-							<label>Enter Symbol: </label><input type='text' name='symbol[]' class='symbol' value='<?php echo getGet('symbol', $i); ?>' /><br />
-							<label>Enter Investment: </label><input type='text' name='capital[]' class='capital' value='<?php echo getGet('capital', $i); ?>' /><br />
+							<label>Enter Symbol: </label><input type='text' name='symbol[]' class='symbol' value='<?php echo Util::getGet('symbol', $i); ?>' /><br />
+							<label>Enter Investment: </label><input type='text' name='capital[]' class='capital' value='<?php echo Util::getGet('capital', $i); ?>' /><br />
 						</div>
 <?php
 					} 
@@ -84,10 +153,41 @@ if(isset($_GET['symbol'])){
 		</div>
 		<div id='results'>
 <?php 
-		if($results){
-//			foreach($years as $year) {
-				echo $years->prettyDump();
-//			}
+		if(Util::getStockCount()){
+?>
+			<table cellpadding='2' cellspacing='2' border='1'>
+			<tr>
+				<th class='blank'>&nbsp;</th>
+<?php
+			for($i = 0; $i < Util::getStockCount(); $i++){
+				echo "<th colspan='".count($print_cols)."'>" . strtoupper($symbols[$i]) . "</th>\n";
+			}
+?>
+				</tr>
+				<tr>
+					<th>Date</th>
+<?php
+					foreach($stocks as $count){
+						foreach($print_cols as $col => $name){
+							echo "<th>".$name."</th>";
+						}
+					}
+?>
+				</tr>
+<?php
+			foreach($stocks[0] as $index => $row){
+				echo "<tr>";
+				echo "<th class='date'>" . $row[Util::COL_DATE] . "</th>";
+				for($i = 0; $i < Util::getStockCount(); $i++){
+					foreach($print_cols as $col => $name){
+						echo "<td>" . Util::cleanData($col, $stocks[$i][$index][$col]) . "</td>";
+					}
+				}
+				echo "</tr>\n";
+			}
+?>
+			</table>
+<?php
 		}
 ?>
 		</div>
@@ -212,3 +312,8 @@ class TradeDay{
 		return $buffer;
 	}
 }
+
+
+
+
+
